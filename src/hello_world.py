a172c3cae144997c1d24bf9a3e951498a44a2fe6
Jeet Gupta <89960686+JeetGupta26@users.noreply.github.com>
2025-12-15 10:11:21 +0530

    Add another print from GitHub

--- src/hello_world.py
+++ src/hello_world.py
@@ -2,6 +2,7 @@ class HelloWorld:
     @staticmethod
     def main():
         print("Hello from GitHub!")
+        print("Change made on GitHub")
 
 if __name__ == "__main__":
     HelloWorld.main()
